6e9f7292209c91e5678dc141304a5eb54246d939
saul <saul@organicdesign.co.nz>
2022-12-14 11:41:35 +1300

    Rebuild pn-counter code.

--- dist/pn-counter/index.d.ts
+++ dist/pn-counter/index.d.ts
@@ -1,27 +1,8 @@
 import type { Uint8ArrayList } from 'uint8arraylist';
 import type { Codec } from 'protons-runtime';
-export declare enum CounterType {
-    POSITIVE = "POSITIVE",
-    NEGATIVE = "NEGATIVE"
-}
-export declare namespace CounterType {
-    const codec: () => Codec<CounterType>;
-}
-export interface InstanceCount {
-    id: Uint8Array;
-    type: CounterType;
-    int?: number;
-    float?: number;
-    double?: number;
-}
-export declare namespace InstanceCount {
-    const codec: () => Codec<InstanceCount>;
-    const encode: (obj: InstanceCount) => Uint8Array;
-    const decode: (buf: Uint8Array | Uint8ArrayList) => InstanceCount;
-}
 export interface CounterData {
-    pData: Uint8Array;
-    nData: Uint8Array;
+    pData?: Uint8Array;
+    nData?: Uint8Array;
 }
 export declare namespace CounterData {
     const codec: () => Codec<CounterData>;
--- dist/pn-counter/index.js
+++ dist/pn-counter/index.js
@@ -2,95 +2,7 @@
 /* eslint-disable complexity */
 /* eslint-disable @typescript-eslint/no-namespace */
 /* eslint-disable @typescript-eslint/no-unnecessary-boolean-literal-compare */
-import { enumeration, encodeMessage, decodeMessage, message } from 'protons-runtime';
-export var CounterType;
-(function (CounterType) {
-    CounterType["POSITIVE"] = "POSITIVE";
-    CounterType["NEGATIVE"] = "NEGATIVE";
-})(CounterType || (CounterType = {}));
-var __CounterTypeValues;
-(function (__CounterTypeValues) {
-    __CounterTypeValues[__CounterTypeValues["POSITIVE"] = 0] = "POSITIVE";
-    __CounterTypeValues[__CounterTypeValues["NEGATIVE"] = 1] = "NEGATIVE";
-})(__CounterTypeValues || (__CounterTypeValues = {}));
-(function (CounterType) {
-    CounterType.codec = () => {
-        return enumeration(__CounterTypeValues);
-    };
-})(CounterType || (CounterType = {}));
-export var InstanceCount;
-(function (InstanceCount) {
-    let _codec;
-    InstanceCount.codec = () => {
-        if (_codec == null) {
-            _codec = message((obj, w, opts = {}) => {
-                if (opts.lengthDelimited !== false) {
-                    w.fork();
-                }
-                if (opts.writeDefaults === true || (obj.id != null && obj.id.byteLength > 0)) {
-                    w.uint32(10);
-                    w.bytes(obj.id);
-                }
-                if (opts.writeDefaults === true || (obj.type != null && __CounterTypeValues[obj.type] !== 0)) {
-                    w.uint32(16);
-                    CounterType.codec().encode(obj.type, w);
-                }
-                if (obj.int != null) {
-                    w.uint32(24);
-                    w.uint32(obj.int);
-                }
-                if (obj.float != null) {
-                    w.uint32(37);
-                    w.float(obj.float);
-                }
-                if (obj.double != null) {
-                    w.uint32(41);
-                    w.double(obj.double);
-                }
-                if (opts.lengthDelimited !== false) {
-                    w.ldelim();
-                }
-            }, (reader, length) => {
-                const obj = {
-                    id: new Uint8Array(0),
-                    type: CounterType.POSITIVE
-                };
-                const end = length == null ? reader.len : reader.pos + length;
-                while (reader.pos < end) {
-                    const tag = reader.uint32();
-                    switch (tag >>> 3) {
-                        case 1:
-                            obj.id = reader.bytes();
-                            break;
-                        case 2:
-                            obj.type = CounterType.codec().decode(reader);
-                            break;
-                        case 3:
-                            obj.int = reader.uint32();
-                            break;
-                        case 4:
-                            obj.float = reader.float();
-                            break;
-                        case 5:
-                            obj.double = reader.double();
-                            break;
-                        default:
-                            reader.skipType(tag & 7);
-                            break;
-                    }
-                }
-                return obj;
-            });
-        }
-        return _codec;
-    };
-    InstanceCount.encode = (obj) => {
-        return encodeMessage(obj, InstanceCount.codec());
-    };
-    InstanceCount.decode = (buf) => {
-        return decodeMessage(buf, InstanceCount.codec());
-    };
-})(InstanceCount || (InstanceCount = {}));
+import { encodeMessage, decodeMessage, message } from 'protons-runtime';
 export var CounterData;
 (function (CounterData) {
     let _codec;
@@ -100,11 +12,11 @@ export var CounterData;
                 if (opts.lengthDelimited !== false) {
                     w.fork();
                 }
-                if (opts.writeDefaults === true || (obj.pData != null && obj.pData.byteLength > 0)) {
+                if (obj.pData != null) {
                     w.uint32(10);
                     w.bytes(obj.pData);
                 }
-                if (opts.writeDefaults === true || (obj.nData != null && obj.nData.byteLength > 0)) {
+                if (obj.nData != null) {
                     w.uint32(18);
                     w.bytes(obj.nData);
                 }
@@ -112,10 +24,7 @@ export var CounterData;
                     w.ldelim();
                 }
             }, (reader, length) => {
-                const obj = {
-                    pData: new Uint8Array(0),
-                    nData: new Uint8Array(0)
-                };
+                const obj = {};
                 const end = length == null ? reader.len : reader.pos + length;
                 while (reader.pos < end) {
                     const tag = reader.uint32();
