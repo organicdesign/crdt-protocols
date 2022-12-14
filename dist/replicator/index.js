68f6f9ebca01981efedfc3e21261e4db4e9cf92a
saul <saul@organicdesign.co.nz>
2022-12-14 13:56:03 +1300

    Create replicator protocol.

---
+++ dist/replicator/index.js
@@ -0,0 +1,57 @@
+/* eslint-disable import/export */
+/* eslint-disable complexity */
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/no-unnecessary-boolean-literal-compare */
+import { encodeMessage, decodeMessage, message } from 'protons-runtime';
+export var CRDTSync;
+(function (CRDTSync) {
+    let _codec;
+    CRDTSync.codec = () => {
+        if (_codec == null) {
+            _codec = message((obj, w, opts = {}) => {
+                if (opts.lengthDelimited !== false) {
+                    w.fork();
+                }
+                if (opts.writeDefaults === true || obj.name !== '') {
+                    w.uint32(10);
+                    w.string(obj.name);
+                }
+                if (opts.writeDefaults === true || (obj.data != null && obj.data.byteLength > 0)) {
+                    w.uint32(18);
+                    w.bytes(obj.data);
+                }
+                if (opts.lengthDelimited !== false) {
+                    w.ldelim();
+                }
+            }, (reader, length) => {
+                const obj = {
+                    name: '',
+                    data: new Uint8Array(0)
+                };
+                const end = length == null ? reader.len : reader.pos + length;
+                while (reader.pos < end) {
+                    const tag = reader.uint32();
+                    switch (tag >>> 3) {
+                        case 1:
+                            obj.name = reader.string();
+                            break;
+                        case 2:
+                            obj.data = reader.bytes();
+                            break;
+                        default:
+                            reader.skipType(tag & 7);
+                            break;
+                    }
+                }
+                return obj;
+            });
+        }
+        return _codec;
+    };
+    CRDTSync.encode = (obj) => {
+        return encodeMessage(obj, CRDTSync.codec());
+    };
+    CRDTSync.decode = (buf) => {
+        return decodeMessage(buf, CRDTSync.codec());
+    };
+})(CRDTSync || (CRDTSync = {}));
